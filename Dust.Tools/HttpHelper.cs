05c37421d62963356c94fe653c7db3dab8869850
dust <dustwork@outlook.com>
2025-03-24 14:44:58 +0800

    增加全局操作

--- Dust.Tools/HttpHelper.cs
+++ Dust.Tools/HttpHelper.cs
@@ -11,6 +11,7 @@ using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Newtonsoft.Json;
 
 namespace Dust.Tools
@@ -22,6 +23,7 @@ namespace Dust.Tools
         public string Message { get; set; }
 
         public Exception E { get; set; }
+
         public string ResultString { get; set; }
 
         public dynamic ResultDynamic { get; set; }
@@ -40,46 +42,73 @@ namespace Dust.Tools
         }
     }
 
-    /*
-         var http = new HttpHelper();
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(HttpMethod.Get, "https://reqres.in/api/users?page=2");
+    public interface IHttpRequestHandle
+    {
+        /// <summary>
+        /// 全局配置拦截
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        void Config(HttpRequestMessage config);
 
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(
-                    HttpMethod.Post,
-                    "https://reqres.in/api/register",
-                    new { email = "eve.holt@reqres.in", password = "pistol" }
-                );
+        /// <summary>
+        /// 全局成功拦截
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
 
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(
-                    HttpMethod.Put,
-                    "https://reqres.in/api/users/2",
-                    new { name = "morpheus", job = "zion resident" }
-                );
-    */
+        void ResponseSuccess(ResponseBody response);
+
+        /// <summary>
+        /// 全局异常拦截
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        void ResponseError(ResponseBody response);
+    }
+
+    public static class HttpRegister
+    {
+        private static IHttpRequestHandle httpRequestHandle;
+
+        public static void Init(IHttpRequestHandle httpRequest)
+        {
+            httpRequestHandle = httpRequest;
+        }
+
+        /// <summary>
+        /// 全局拦截
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+
+        public static void Config(HttpRequestMessage config)
+        {
+            httpRequestHandle?.Config(config);
+        }
+
+        /// <summary>
+        /// 全局成功拦截
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+
+        public static void ResponseSuccess(ResponseBody response)
+        {
+            httpRequestHandle?.ResponseSuccess(response);
+        }
+
+        /// <summary>
+        /// 全局异常拦截
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static void ResponseError(ResponseBody response)
+        {
+            httpRequestHandle?.ResponseError(response);
+        }
+    }
+
     /// <summary>
     /// 请求类
     /// </summary>
@@ -89,38 +118,17 @@ namespace Dust.Tools
 
         private Action<ResponseBody> ErrorCallBack;
 
-        private Action<HttpRequestMessage> StartCallBack;
+        private Action<HttpContent> SetContentCallBack;
 
-        private static Dictionary<string, string> headerCollection;
-
-        private static readonly char splitChart = '&';
+        private Action<HttpRequestHeaders> SetHeaderCallBack;
 
         public ResponseBody Body { get; set; }
 
-        public HttpHelper SetHeaders(Dictionary<string, string> headers)
-        {
-            headerCollection = headers;
-            return this;
-        }
-
-        public HttpHelper OnSuccess(Action<ResponseBody> callBack)
-        {
-            SuccessCallBack = callBack;
-            return this;
-        }
-
-        public HttpHelper OnError(Action<ResponseBody> callBack)
-        {
-            ErrorCallBack = callBack;
-            return this;
-        }
-
-        public HttpHelper OnStart(Action<HttpRequestMessage> callBack)
-        {
-            StartCallBack = callBack;
-            return this;
-        }
-
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <param name="httpMethod"></param>
+        /// <param name="url"></param>
         public void Send(HttpMethod httpMethod, string url)
         {
             try
@@ -135,11 +143,56 @@ namespace Dust.Tools
                     Message = "错误：" + ex.Message,
                     E = ex,
                 };
+                HttpRegister.ResponseError(Body);
                 ErrorCallBack?.Invoke(Body);
             }
         }
 
-        public void Send(HttpMethod httpMethod, string url, dynamic data)
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <param name="httpMethod"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+
+        public void Send(HttpMethod httpMethod, string url, MultipartFormDataContent data)
+        {
+            try
+            {
+                SetContentCallBack = (
+                    content =>
+                    {
+                        content = data;
+                    }
+                );
+                RequestAsync(url, httpMethod).Wait();
+            }
+            catch (Exception ex)
+            {
+                Body = new ResponseBody
+                {
+                    Code = -1,
+                    Message = "错误：" + ex.Message,
+                    E = ex,
+                };
+                HttpRegister.ResponseError(Body);
+                ErrorCallBack?.Invoke(Body);
+            }
+        }
+
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <param name="httpMethod"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <param name="contentType"></param>
+        public void Send(
+            HttpMethod httpMethod,
+            string url,
+            dynamic data,
+            string contentType = "application/json"
+        )
         {
             try
             {
@@ -148,37 +201,19 @@ namespace Dust.Tools
                 {
                     dataParams = JsonConvert.SerializeObject(data);
                 }
-                RequestAsync(url, httpMethod, dataParams).Wait();
-            }
-            catch (Exception ex)
-            {
-                Body = new ResponseBody
-                {
-                    Code = -1,
-                    Message = "错误：" + ex.Message,
-                    E = ex,
-                };
-                ErrorCallBack?.Invoke(Body);
-            }
-        }
 
-        public void Send(HttpMethod httpMethod, string url, NameValueCollection data)
-        {
-            try
-            {
-                string dataParams = string.Empty;
-                if (data != null)
-                {
-                    string text = string.Empty;
-                    string[] allKeys = data.AllKeys;
-                    foreach (string text2 in allKeys)
+                SetContentCallBack = (
+                    content =>
                     {
-                        string text3 = data[text2];
-                        text = text + "&" + text2.ToLower() + "=" + text3;
+                        StringContent strcontent = new StringContent(
+                            data,
+                            Encoding.UTF8,
+                            contentType
+                        );
+                        content = strcontent;
                     }
-                    dataParams = text.TrimStart(new char[1] { splitChart });
-                }
-                RequestAsync(url, httpMethod, dataParams).Wait();
+                );
+                RequestAsync(url, httpMethod).Wait();
             }
             catch (Exception ex)
             {
@@ -188,50 +223,102 @@ namespace Dust.Tools
                     Message = "错误：" + ex.Message,
                     E = ex,
                 };
+                HttpRegister.ResponseError(Body);
                 ErrorCallBack?.Invoke(Body);
             }
         }
 
-        public void Send(HttpMethod httpMethod, string url, string data)
-        {
-            try
-            {
-                RequestAsync(url, httpMethod, data).Wait();
-            }
-            catch (Exception ex)
-            {
-                Body = new ResponseBody
-                {
-                    Code = -1,
-                    Message = "错误：" + ex.Message,
-                    E = ex,
-                };
-                ErrorCallBack?.Invoke(Body);
-            }
-        }
-
-        private async Task RequestAsync(
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <param name="httpMethod"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <param name="contentType"></param>
+        public void Send(
+            HttpMethod httpMethod,
             string url,
-            HttpMethod method,
-            string data = null,
+            string data,
             string contentType = "application/json"
         )
         {
+            try
+            {
+                SetContentCallBack = (
+                    content =>
+                    {
+                        StringContent strcontent = new StringContent(
+                            data,
+                            Encoding.UTF8,
+                            contentType
+                        );
+                        content = strcontent;
+                    }
+                );
+                RequestAsync(url, httpMethod).Wait();
+            }
+            catch (Exception ex)
+            {
+                Body = new ResponseBody
+                {
+                    Code = -1,
+                    Message = "错误：" + ex.Message,
+                    E = ex,
+                };
+                HttpRegister.ResponseError(Body);
+                ErrorCallBack?.Invoke(Body);
+            }
+        }
+
+        /// <summary>
+        /// 设置请求头
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+
+        public HttpHelper SetHeaders(Action<HttpRequestHeaders> headers)
+        {
+            SetHeaderCallBack = headers;
+            return this;
+        }
+
+        /// <summary>
+        /// 请求成功
+        /// </summary>
+        /// <param name="callBack"></param>
+        /// <returns></returns>
+
+        public HttpHelper OnSuccess(Action<ResponseBody> callBack)
+        {
+            SuccessCallBack = callBack;
+            return this;
+        }
+
+        /// <summary>
+        /// 请求失败
+        /// </summary>
+        /// <param name="callBack"></param>
+        /// <returns></returns>
+
+        public HttpHelper OnError(Action<ResponseBody> callBack)
+        {
+            ErrorCallBack = callBack;
+            return this;
+        }
+
+        /// <summary>
+        /// 请求方法
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        private async Task RequestAsync(string url, HttpMethod method)
+        {
             using (HttpRequestMessage request = new HttpRequestMessage(method, url))
             {
-                if (headerCollection != null && headerCollection.Count > 0)
-                {
-                    foreach (var item in headerCollection)
-                    {
-                        request.Headers.Add(item.Key, item.Value);
-                    }
-                }
-                if (data != null)
-                {
-                    StringContent strcontent = new StringContent(data, Encoding.UTF8, contentType);
-                    request.Content = strcontent;
-                }
-                StartCallBack?.Invoke(request);
+                SetHeaderCallBack?.Invoke(request.Headers);
+                SetContentCallBack?.Invoke(request.Content);
+                HttpRegister.Config(request);
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
                 using (HttpResponseMessage response = await client.SendAsync(request))
@@ -254,11 +341,13 @@ namespace Dust.Tools
                             Body.ResultDynamic = JsonConvert.DeserializeObject(Body.ResultString);
                         }
                         catch (Exception) { }
+                        HttpRegister.ResponseSuccess(Body);
                         SuccessCallBack?.Invoke(Body);
                         return;
                     }
                 }
             }
+            HttpRegister.ResponseError(Body);
             ErrorCallBack?.Invoke(Body);
         }
     }
--- Dust.Tools/Program.cs
+++ Dust.Tools/Program.cs
@@ -1,55 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dust.Tools
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            var http = new HttpHelper();
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(HttpMethod.Get, "https://reqres.in/api/users?page=2");
-
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(
-                    HttpMethod.Post,
-                    "https://reqres.in/api/register",
-                    new { email = "eve.holt@reqres.in", password = "pistol" }
-                );
-
-            http.OnSuccess(res =>
-                {
-                    Console.WriteLine("OnSuccess ...");
-                })
-                .OnError(res =>
-                {
-                    Console.WriteLine("OnError ...");
-                })
-                .Send(
-                    HttpMethod.Put,
-                    "https://reqres.in/api/users/2",
-                    new { name = "morpheus", job = "zion resident" }
-                );
-        }
+        static void Main(string[] args) { }
     }
 }
